151b8e197fb1ead70809c485827ba36d9fada01f
kaankarakose <naakkarakose@gmail.com>
2025-09-25 10:18:35 +0200

    discarded 'dont care' saving in result file

--- examples/utils.py
+++ examples/utils.py
@@ -9,15 +9,12 @@ def write_mot_results(txt_path, results, frame_idx):
     nr_dets = len(results.boxes)
     frame_idx = torch.full((1, 1), frame_idx + 1)
     frame_idx = frame_idx.repeat(nr_dets, 1)
-    dont_care = torch.full((nr_dets, 1), -1)
     mot = torch.cat([
         frame_idx,
         results.boxes.id.unsqueeze(1).to('cpu'),
         results.boxes.xyxy.to('cpu'),
         results.boxes.conf.unsqueeze(1).to('cpu'),
-        results.boxes.cls.unsqueeze(1).to('cpu'),
-        dont_care
-    ], dim=1)
+        results.boxes.cls.unsqueeze(1).to('cpu')], dim=1)
     # create parent folder
     txt_path.parent.mkdir(parents=True, exist_ok=True)
     # create mot txt file
